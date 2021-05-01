66cce2f11ec593bcf92d75d8f99116358b465e13
Md Taseen Atehar <63313585+Seen3@users.noreply.github.com>
2021-05-01 15:25:29 +0530

    Update pp.js

--- pp.js
+++ pp.js
@@ -1,11 +1,1 @@
-function tog(){
-  document.getElementById('ngo').classList.toggle("a")
-}
-let ngo=document.querySelector("#ngo");
-let home=document.querySelector("#home"); 
-ngo.addEventListener("click", ()=>{
-  this.classList.toggle("active");
-})
-home.addEventListener("click",()=>{
-  this.classList.toggle("active");
-})
+
