d38a9a0c94509b99e51b35869b86671a7e387061
Mohamed Ayaan <72858215+Mohamed-Ayaan358@users.noreply.github.com>
2021-05-01 21:35:02 +0530

    Update pp.js

--- pp.js
+++ pp.js
@@ -1,1 +1,16 @@
-
+function villagers(){
+	var email=document.getElementbyId("email").value
+	var pass=document.getElementbyId("pass").value
+	axios({
+		method: 'post',
+		url: 'https://localhost:8000/',
+		data: {
+			id : email,
+			password : pass,
+		}
+	}).then(response => {
+		var store=response.data
+		var firstdiv= document.getElementById("created")
+		firstdiv.innerHTML= store
+	})
+}
