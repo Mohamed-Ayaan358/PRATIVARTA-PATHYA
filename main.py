324a171add5d059bb309834db931e1101194a6bc
ArnavKumar7 <78247827+ArnavKumar7@users.noreply.github.com>
2021-05-02 01:59:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,14 +16,14 @@ pp.add_middleware(
     allow_headers=['*'],
 )
 
-@app.post('/users')
+@pp.post('/users')
 async def create_user(user: Villagers):
     if hasattr(user, 'id'):
         delattr(user, 'id')
     ret = db.users.insert_one(user.dict(by_alias=True))
     user.id = ret.inserted_id
     return {'user': user}
-@app.get('/details')
+@pp.get('/details')
 async def list_users():
     users = []
     for user in db.users.find():
