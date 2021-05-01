feb8ffc03922fd3ae5679940d62d0a2709485c3a
Mohamed Ayaan <72858215+Mohamed-Ayaan358@users.noreply.github.com>
2021-05-01 21:43:17 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.encoders import jsonable_encoder
+from fastapi.middleware.cors import CORSMiddleware
+import random
+from datetime import datetime
+import json
+from fastapi.responses import HTMLResponse
+
+pp = FastAPI()
+
+origins = ["*"]
+pp.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+
+class villagers(BaseModel):
+   email: str
+   password: str
+
+
+@pp.get('/home')
+def signup(villagers: villagers):
+	enc = jsonable_encoder(villagers)
+	v_name = enc['email']
+	password = enc['password']
+	d = {
+	    "email": v_name,
+	    "password": password,
+        }
+	return 'hello '+v_name+'you have been registered,go to the Login page to Login'
